f33497679ebd2cd72f5b612977f641f042400963
YUN-CHENG <608556007@alum.ccu.edu.tw>
2023-04-24 16:37:20 +0800

    update: aboutpage

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,8 +1,48 @@
 import React from 'react'
+import { StyledAbout } from './styles/About'
+import { FaFileDownload } from 'react-icons/fa'
 
 function About() {
   return (
-    <div>About</div>
+    <StyledAbout>
+      <div className="about section" id='about'>
+        <h2 className="section-title">About Me</h2>
+        <span className="section-subtitle">My Introduction</span>
+        <div className="about-container container grid">
+          <img src=".././images/sitting2.png" alt="" className='about-img' />
+
+          <div className="about-data">
+            <p className="about-description">
+            我是吳昀澄，是一位在醫院工作的程式設計師，主要使用C#、ASP.NET和SQL等程式語言開發醫院申報程式。
+            </p>
+            <div className="about-info">
+              <div>
+                <span className='about-info-title'>08+</span>
+                <span className='about-info-name'>Years <br/> experience</span>
+              </div>
+
+              <div>
+                <span className='about-info-title'>08+</span>
+                <span className='about-info-name'>Completed <br/> project</span>
+              </div>
+
+              <div>
+                <span className='about-info-title'>05+</span>
+                <span className='about-info-name'>Companies <br/> worked</span>
+              </div>
+            </div>
+            
+            <div className="about-buttons">
+              <a href="/#" className="button button-flex">
+              Download CV<FaFileDownload className='button-icon'/>
+              </a>
+            </div>
+
+          </div>
+        </div>
+      </div>
+    </StyledAbout>
+    
   )
 }
 
---
+++ src/components/styles/About.js
@@ -0,0 +1,39 @@
+import styled from "styled-components";
+
+export const StyledAbout = styled.section`
+.about-img{
+    width: 200px;
+    border-radius: .5rem;
+    justify-self: center;
+    align-self: center;
+}
+.about-description{
+    text-align:center;
+    margin-bottom: var(--mb-2-5);
+}
+.about-info{
+    display:flex;
+    justify-content: space-between;
+    margin-bottom: var(--mb-2-5);
+}
+.about-info-title{
+    font-size: var(--h2-font-size);
+    font-weight: var(--font-semi-bold);
+    color: var(--title-color);
+}
+
+.about-info-name{
+    font-size: var(--smaller-font-size);
+}
+
+.about-info-title,
+.about-info-name{
+    display: block;
+    text-align: center;
+}
+
+.about-buttons{
+    display: flex;
+    justify-content:center;
+}
+` 
